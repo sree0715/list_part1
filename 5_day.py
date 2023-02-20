2417af84773f73465e0062ad5fff76a8422f71ad
sree0715 <125061287+sree0715@users.noreply.github.com>
2023-02-20 22:03:11 +0530

    Add files via upload

---
+++ 5_day.py
@@ -0,0 +1,63 @@
+# a=[]
+# print(type(a))
+
+# print(dir(list))
+# b=["10","20","30"]
+# print(b.append(10))
+# print(b)
+
+# b[0]=23"
+# print(b)
+
+# print(b.clear())
+# print(b)
+
+# c=[4,3.1,"hello",[5,6,7]]
+# c[0]=6
+# print(c)
+
+# d=('"3","5","7","9"')
+# e=['h','e','l','l','o']
+# print(d.index("0"))
+# print(e.index("k"))
+
+# print(e.count("8"))
+
+# f=[1,"h",'j',[10,76]]
+# f[0]=7
+# f[7]="o"
+# print(f)
+# print(f[0],type(f[3][0]))
+
+# g=[8,4,9,6]
+# print(g.copy())
+
+# h=[9,5,3,0,1]
+# i=[66,77,34,0]
+# h.extend(i)
+# print(h)
+# print(type(h))
+# k=[4,6,3]
+# k.extend(h)
+# print(k)
+
+# l="hello"
+# print(l.rindex("l"))
+# print(l.find("l"))
+
+# t=["hello" ,"python", "programming"]
+# print(t.count("hello"))
+
+# s=["hello python programming"]
+# print(s.index('o',5))
+
+# t.pop(0)
+# print(t)
+
+
+
+
+
+
+
+
